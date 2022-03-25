b8d99dc8cf547c853f7e60310cdb58dcb6683d33
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2022-03-25 16:34:48 -0300

    Layout e printando o tabuleiro

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,13 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
-
 }
